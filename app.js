1fa4adbaeebe53a2a871ddc3a383377921e8bad8
vinay <vinayambekar.b@gmail.com>
2024-04-07 21:11:11 +0530

    'header-done'

--- app.js
+++ app.js
@@ -11,21 +11,27 @@ window.addEventListener('scroll', () => {
 /* scrolling up end*/
 
 /* timer starts */
-let min=document.querySelectorAll('#minutes')
-let sec=document.querySelectorAll('#seconds')
-console.log(min,sec)
-let StartingMinuts=15;
-let time=StartingMinuts*60;
-setInterval(updateCount,1000)
-function updateCount(){
- let minuts=Math.floor(time/60);
- let seconds=time%60
- for(let i=0;i<=1;i++){
-    min[i].textContent=`${minuts}`
-    sec[i].textContent=`${seconds}`
- }
- time--
+let min = document.querySelectorAll('#minutes');
+let sec = document.querySelectorAll('#seconds');
+console.log(min, sec);
+let startingMinutes = 15;
+let time = startingMinutes * 60;
+let intervalId = setInterval(updateCount, 1000);
+
+function updateCount() {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    for (let i = 0; i <= 1; i++) {
+        min[i].textContent = `${minutes}`;
+        sec[i].textContent = `${seconds}`;
+    }
+    if (time === 0) {
+        clearInterval(intervalId); // Stop the timer when it reaches 0
+    } else {
+        time--;
+    }
 }
+
 /* timer end */
 
 
